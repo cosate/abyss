146bbe8f55e9bbd7a715b134935a61f73cb4acc3
gaofeijin <397830271@qq.com>
2017-10-22 17:38:08 +0800

    add some pages

--- test/test.cpp
+++ test/test.cpp
@@ -8,11 +8,14 @@
 #include<sys/socket.h>
 #include<linux/tcp.h>
 #include<arpa/inet.h>
+#include<sys/epoll.h>
+#include<sys/time.h>
+#include<sys/types.h>
+
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-
 	addrinfo hints, *res, *res_save;
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_flags = AI_PASSIVE;
@@ -24,14 +27,40 @@ int main(int argc, char* argv[])
 
 	char ips[16];
 	int ret = getaddrinfo(NULL, port, &hints, &res);
+
+	int listen_fd;
 	
 	for(res_save = res; res != NULL; res = res->ai_next)
 	{
-		inet_ntop(AF_INET, &(((struct sockaddr_in *)(res->ai_addr))->sin_addr), ips, 16);
-		printf("%s\n", ips);
-		printf("%d\n", ntohs(((struct sockaddr_in *)(res->ai_addr))->sin_port));
+		listen_fd = socket(res->ai_family, res->ai_socktype | SOCK_NONBLOCK, res->ai_protocol);
+		bind(listen_fd, res->ai_addr, res->ai_addrlen);
+		listen(listen_fd, 1024);
+		break;
 	}
 
 	freeaddrinfo(res_save);
+
+	cout<<"listen fd "<<listen_fd<<endl;
+
+
+	int epfd = epoll_create1(0);
+
+	epoll_event listen_event;
+	listen_event.events = EPOLLIN;
+	listen_event.data.fd = listen_fd;
+
+	epoll_ctl(epfd, EPOLL_CTL_ADD, listen_fd, &listen_event);
+
+	epoll_event results[1024];
+
+	for(;;)
+	{
+		int nfds = epoll_wait(epfd, results, 1024, 500);
+		for(int i = 0; i < nfds; i++)
+		{
+			int res = results[i].data.fd;
+			cout<<"res "<<res<<endl;
+		}
+	}
 	return 0;
 }
