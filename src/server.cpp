dced016d39e4cdc388acb153048da07681b68d8e
gaofeijin <397830271@qq.com>
2018-02-26 14:00:19 +0800

    little change

--- src/server.cpp
+++ src/server.cpp
@@ -8,6 +8,7 @@
 #include<sys/time.h>
 #include<sys/epoll.h>
 #include<cstring>
+#include<sys/resource.h>
 
 #include<vector>
 #include<algorithm>
@@ -24,20 +25,178 @@ vector<ConnectionData*> connections;
 Config server_config;
 int epfd;
 
-void server_init()
+static void print_usage()
 {
-	load_config(server_config, "config.json");
+	printf("Usage : abyss [option]\n"
+		   "    --stop    Stop abyss.\n"
+		   "    --restart Restart abyss and reload config.json.\n"
+		   "    --help    Print usage.\n");
+}
+
+static pid_t get_pid()
+{
+	FILE * fp = fopen("log/abyss.pid", "r");
+	if(!fp)
+	{
+		fprintf(stderr, "[EXIT in %s:%d] %s\n", __FILE__, __LINE__, "open pid file: log/abyss.pid failed");
+		exit(-1);
+	}
+	pid_t pid = 0;
+	fscanf(fp, "%d", &pid);
+	fclose(fp);
+	return pid;
+}
+
+static void save_pid(pid_t pid)
+{
+	FILE *fp = fopen("log/abyss.pid", "w");
+	if(!fp)
+	{
+		fprintf(stderr, "[EXIT in %s:%d] %s\n", __FILE__, __LINE__, "open pid file: log/abyss.pid failed");
+		exit(-1);
+	}
+	fprintf(fp, "%d", pid);
+	fclose(fp);
+}
+
+static void send_signal(int sig)
+{
+	pid_t pid = get_pid();
+	if(pid == 0)
+	{
+		fprintf(stderr, "[EXIT in %s:%d] %s\n", __FILE__, __LINE__, "abyss is not running");
+		exit(-1);
+	}
+	kill(pid, sig);
+}
+
+enum class abyss_status {ABYSS_STOP = 0, ABYSS_RELOAD};
+static abyss_status status = abyss_status::ABYSS_STOP;
+
+static void sigint_handler(int sig)
+{
+	abyss_log("abyss exited");
+	switch(status)
+	{
+		case abyss_status::ABYSS_STOP:
+		{
+			kill(-get_pid(), SIGINT);
+			save_pid(0);
+			raise(SIGKILL);
+			break;
+		}
+		case abyss_status::ABYSS_RELOAD:
+		{
+			status = abyss_status::ABYSS_STOP;
+			break;
+		}
+		default:
+		{
+			fprintf(stderr, "[EXIT in %s:%d] %s\n", __FILE__, __LINE__, "invalid reload_flag in sigint_handler");
+			exit(-1);
+		}
+	}
+}
+
+static void sighup_handler(int sig)
+{
+	abyss_log("abyss reload config.json and restart");
+	close(server_config.src_root);
+	close(server_config.err_root);
+	if(load_config(server_config, "config.json") != ABYSS_OK)
+	{
+		fprintf(stderr, "[EXIT in %s:%d] %s\n", __FILE__, __LINE__, "load config.json failed");
+		exit(-1);
+	}
+	status = abyss_status::ABYSS_RELOAD;
+	kill(-get_pid(), SIGINT);
+}
+
+static void set_sig_handler(int sig, void(*handler)(int))
+{
+	struct sigaction sa;
+	sa.sa_handler = handler;
+	sa.sa_flags = SA_RESTART;
+	sigemptyset(&sa.sa_mask);
+	if(sigaction(sig, &sa, NULL) == -1)
+	{
+		fprintf(stderr, "[EXIT in %s:%d] %s\n", __FILE__, __LINE__, "set signal handler failed");
+		exit(-1);
+	}
+}
+
+static void server_init()
+{
+	if(load_config(server_config, "config.json") != ABYSS_OK)
+	{
+		fprintf(stderr, "[EXIT in %s:%d] %s\n", __FILE__, __LINE__, "load config.json failed");
+		exit(-1);
+	}
+	signal(SIGPIPE, SIG_IGN);
+	set_sig_handler(SIGINT, sigint_handler);
+	set_sig_handler(SIGTERM, sigint_handler);
+	set_sig_handler(SIGHUP, sighup_handler);
+	struct rlimit nofile_limit = { 65535, 65535};
+	if(setrlimit(RLIMIT_NOFILE, &nofile_limit) == -1)
+	{
+		fprintf(stderr, "[EXIT in %s:%d] %s\n", __FILE__, __LINE__, strerror(errno));
+		exit(-1);
+	}
+
 	make_heap(connections.begin(), connections.end(), cmp);
 	epfd = epoll_create1(0);
 	RequestHeader::init_field_position();
 	Response::init_code_description();
 	Response::init_mime();
-}
 
+	if(server_config.daemon)
+		daemon(1, 0);
+
+	save_pid(getpid());
+}
 
 int main(int argc, char* argv[])
 {
+	if(argc >= 2)
+	{
+		if(!strncmp(argv[1], "--stop", 6))
+			send_signal(SIGINT);
+		else if(!strncmp(argv[1], "--restart", 9))
+			send_signal(SIGHUP);
+		else
+			print_usage();
+		exit(0);
+	}
+
+	if(get_pid() != 0)
+	{
+		fprintf(stderr, "[EXIT in %s:%d] %s\n", __FILE__, __LINE__, "abyss is running");
+		exit(-1);
+	}
+	get_pid();
 	server_init();
+	abyss_log("abyss server started, listening at port: %u", server_config.port);
+	
+	int worker = 0;
+	for(;;)
+	{
+		if(worker >= server_config.worker)
+		{
+			wait(NULL);
+			worker--;
+			abyss_log("abyss subprocess failed, restarting");
+		}
+
+		pid_t pid = fork();
+		if(pid == 0)
+			break;
+		else if(pid < 0)
+			continue;
+		worker++;
+	}
+
+	signal(SIGINT, SIG_DFL);
+	signal(SIGHUP, SIG_DFL);
 	
 	int listen_fd = create_listen_socket(NULL, server_config.port, 1024);
 	if(listen_fd == ABYSS_ERR)
